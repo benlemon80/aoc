cbd6b288f4997a5f16c6cca9ebfe6f97e888a74e
benlemon80 <114302198+benlemon80@users.noreply.github.com>
2022-12-03 12:00:50 +0000

    day 2 part 2

--- day_3/src/main.rs
+++ day_3/src/main.rs
@@ -5,25 +5,33 @@ fn main() {
         'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
     ];
 
-    let sum: i32 = include_str!("input.txt")
-        .lines()
-        .map(|l| {
-            let (comp_1, comp_2) = l.split_at(l.len() / 2);
+    let rucksacks: Vec<&str> = include_str!("input.txt").lines().collect();
 
-            let mut shared: Vec<char> = comp_1
+    let part_1: i32 = rucksacks
+        .iter()
+        .map(|l| {
+            let (one, two) = l.split_at(l.len() / 2);
+
+            let shared: Vec<char> = one
                 .chars()
-                .filter(|c1| comp_2.chars().any(|c2| &c2 == c1))
+                .filter(|c1| two.chars().any(|c2| &c2 == c1))
                 .collect();
 
-            shared.sort();
-            shared.dedup();
-
-            shared
-                .iter()
-                .map(|c| priorities.iter().position(|d| d == c).unwrap() as i32 + 1)
-                .sum::<i32>()
+            priorities.iter().position(|&d| d == shared[0]).unwrap() as i32 + 1
         })
         .sum();
 
-    println!("{sum}");
+    let part_2: i32 = rucksacks
+        .chunks(3)
+        .map(|g| {
+            let badge: Vec<char> = g[0]
+                .chars()
+                .filter(|c1| g[1].chars().any(|c2| &c2 == c1) && g[2].chars().any(|c3| &c3 == c1))
+                .collect();
+
+            priorities.iter().position(|&d| d == badge[0]).unwrap() as i32 + 1
+        })
+        .sum();
+
+    println!("{}, {}", part_1, part_2);
 }
