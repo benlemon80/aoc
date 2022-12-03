5cd7f00616c3c440981642c455d9ec6593c4d20f
benlemon80 <114302198+benlemon80@users.noreply.github.com>
2022-12-03 12:12:29 +0000

    remove unnecessary type declaration

--- day_3/src/main.rs
+++ day_3/src/main.rs
@@ -7,7 +7,7 @@ fn main() {
 
     let rucksacks: Vec<&str> = include_str!("input.txt").lines().collect();
 
-    let part_1: i32 = rucksacks.iter().fold(0, |acc, l| {
+    let part_1 = rucksacks.iter().fold(0, |acc, l| {
         let (one, two) = l.split_at(l.len() / 2);
 
         let shared: Vec<char> = one
@@ -15,16 +15,16 @@ fn main() {
             .filter(|c1| two.chars().any(|c2| &c2 == c1))
             .collect();
 
-        acc + priorities.iter().position(|&d| d == shared[0]).unwrap() as i32 + 1
+        acc + priorities.iter().position(|&d| d == shared[0]).unwrap() + 1
     });
 
-    let part_2: i32 = rucksacks.chunks(3).fold(0, |acc, g| {
+    let part_2 = rucksacks.chunks(3).fold(0, |acc, g| {
         let badge: Vec<char> = g[0]
             .chars()
             .filter(|c1| g[1].chars().any(|c2| &c2 == c1) && g[2].chars().any(|c3| &c3 == c1))
             .collect();
 
-        acc + priorities.iter().position(|&d| d == badge[0]).unwrap() as i32 + 1
+        acc + priorities.iter().position(|&d| d == badge[0]).unwrap() + 1
     });
 
     println!("{}, {}", part_1, part_2);
