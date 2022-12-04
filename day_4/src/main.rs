83ed12092d61f3cca205b6fd5d30a078eda7ada7
benlemon80 <114302198+benlemon80@users.noreply.github.com>
2022-12-04 13:08:22 +0000

    day 4 both parts

---
+++ day_4/src/main.rs
@@ -0,0 +1,28 @@
+fn check_overlaps(n: &Vec<i32>) -> bool {
+    (n[0] < n[2] && n[1] < n[2]) || (n[2] < n[0] && n[3] < n[0])
+}
+
+fn check_contains(n: &Vec<i32>) -> bool {
+    (n[0] >= n[2] && n[1] <= n[3]) || (n[2] >= n[0] && n[3] <= n[1])
+}
+
+fn main() {
+    let digits: Vec<Vec<i32>> = include_str!("input.txt")
+        .lines()
+        .map(|l| {
+            l.split(&[',', '-'])
+                .map(|s| s.parse::<i32>().unwrap())
+                .collect()
+        })
+        .collect();
+
+    let part_1 = digits
+        .iter()
+        .fold(0, |acc, n| acc + if check_contains(&n) { 1 } else { 0 });
+
+    let part_2 = digits
+        .iter()
+        .fold(0, |acc, n| acc + if check_overlaps(&n) { 0 } else { 1 });
+
+    println!("{part_1}, {part_2}")
+}
